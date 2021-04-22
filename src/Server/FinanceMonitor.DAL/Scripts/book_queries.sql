44dd4e420f622666a7a7881adc13dd2cb1ddab51
xtergs <xtergs@gmail.com>
2021-04-22 23:21:02 +0300

    update book_queries.sql

--- src/Server/FinanceMonitor.DAL/Scripts/book_queries.sql
+++ src/Server/FinanceMonitor.DAL/Scripts/book_queries.sql
@@ -1,6 +1,5 @@
 ﻿use TSQLV5
 
-
 Select custId, orderdate, orderid, val,
       val - first_value(val) over(partition by custid
            order by orderdate, orderid
@@ -433,3 +432,427 @@ drop event session xe_window_spool on server
 alter event session xe_window_spool on server state = start;
 
 
+----------------
+go
+use TSQLV5;
+go
+create or alter function dbo.GetNums(@low as bigint, @high as bigint) returns
+    table
+    as 
+    return 
+with 
+     L0 as (select c from (values(1),(1)) as D(c)),
+     L1 as (select 1 as c from L0 as a cross join L0 as B),
+     L2 as (select 1 as c from L1 as a cross join L1 as B),
+     L3 as (select 1 as c from L2 as a cross join L2 as B),
+     L4 as (select 1 as c from L3 as a cross join L3 as B),
+     L5 as (select 1 as c from L4 as a cross join L4 as B),
+     Nums as (select row_number() over (order by (select null)) as rownum from L5)
+select top (@high - @low + 1) @low + rownum - 1 as n
+from Nums
+order by rownum;
+go
+
+select n from dbo.GetNums(11, 20)
+go
+declare 
+    @start as DAte = '20190201',
+    @end as Date = '20190212';
+
+select Dateadd(day, n, @start) as dt
+from dbo.GetNums(0, Datediff(day, @start, @end)) as Nums;
+
+go
+drop table if exists Sales.MyOrders;
+go
+select 0 as orderid, custid, empid, orderdate
+into Sales.MyOrders
+from Sales.Orders;
+
+select * from Sales.MyOrders
+go
+with c as (
+    select orderid, row_number() over (order by orderdate, custid) as rownum
+    from Sales.MyOrders
+)
+update c
+set orderid = rownum;
+go
+drop table if exists dbo.MySequence;
+create table dbo.MySequence(val int);
+insert into dbo.MySequence values(0);
+go
+create or alter procedure dbo.GetSequence
+@val as int output,
+@n as int = 1
+as
+    begin 
+        update MySequence
+        set @val = val + 1,
+            val += @n;
+    end
+go
+
+declare @key as int;
+exec dbo.GetSequence @val = @key output;
+select @key
+
+go
+select custid
+from Sales.Customers
+where country = N'UK';
+
+go
+declare @firstkey as int, @rc as int;
+declare @CustsStage as table(
+    custid int,
+    rownum int
+                            );
+
+insert into @CustsStage(custid, rownum)
+select custid, row_number() over (order by (select null)) as rownum
+from sales.Customers
+where country = N'UK'
+
+set @rc = @@rowcount;
+
+exec dbo.GetSequence @val = @firstkey output, @n = @rc;
+
+select custid, @firstkey + rownum - 1 as keycol
+from @CustsStage
+
+select * from MySequence
+go
+drop procedure if exists dbo.GetSequence;
+drop table if exists dbo.MySequence;
+
+go
+create unique index idx_od_oid_i_cid_eid
+on Sales.Orders(orderdate, orderid)
+include (custid, empid);
+go
+declare @pagenum as int = 3,
+    @pagesize as int = 25;
+
+with c as (
+    select row_number() over (order by orderdate, orderid) as rownum,
+           orderid, orderdate, custid, empid
+    from Sales.Orders
+)
+select orderid, orderdate, custid, empid
+from c
+where rownum between (@pagenum -1) * @pagesize + 1 and @pagenum * @pagesize
+order by rownum;
+go;
+
+drop table if exists Sales.MyOrders;
+go
+
+select * into Sales.MyOrders from Sales.Orders
+Union all
+select * from Sales.Orders where orderid % 100 = 0
+union all
+select * from Sales.Orders where orderid % 50 = 0;
+
+go
+
+select orderid, row_number() over (partition by orderid order by (select null)) as n 
+from Sales.MyOrders;
+go
+with c as
+    (
+        select orderid, row_number() over (partition by orderid order by (select null)) as n
+        from sales.MyOrders
+    )
+delete from C where n> 1;
+go
+
+with c as
+         (
+             select *, row_number() over (partition by orderid order by (select null)) as n
+             from sales.MyOrders
+         )
+select orderid, custid, empid, orderdate, requireddate, shippeddate,
+       shipperid, freight, shipname, shipaddress, shipcity, shipregion,
+       shippostalcode, shipcountry
+into Sales.MyOrdersTmp
+from C
+where n = 1;
+
+-- re-create indexes, constraints
+
+truncate table Sales.MyORders;
+alter table Sales.MyOrdersTmp switch to Sales.MyOrders;
+Drop table Sales.MyOrdersTmp;
+
+go
+
+select orderid,
+       row_number() over(order by orderid) as rownum,
+       rank() over(order by orderid) as rnk
+from Sales.MyOrders
+go
+drop table if exists Sales.MyOrders
+go
+
+with C as (
+    select Year(orderdate) as orderyear, Month(orderdate) as ordermonth, val
+    from Sales.OrderValues
+)
+select *
+from C
+Pivot(sum(val)
+for ordermonth in ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12])) as P;
+go
+with C as (
+    select custid,
+           cast(orderid as varchar(11)) as sorderid,
+           row_number() over (partition by custid order by orderdate desc, orderid desc) as rownum
+    from Sales.OrderValues
+)
+select custid, concat([1], ',' + [2],',' + [3],','+ [4],','+ [5]) as orderids
+from C
+pivot (max(sorderid) for rownum in ([1],[2],[3],[4],[5])) as P;
+go
+
+------
+create unique index idx_cid_odD_oidD_i_empid
+on Sales.Orders(custid, orderdate desc, orderid desc)
+include (empid)
+go
+with C as (
+    select custid, orderdate, orderid, empid, 
+    row_number()  over (partition by custid order by orderdate desc, orderid desc) as rn
+    from Sales.Orders
+    )
+select custid, orderdate, orderid, empid, rn
+from C
+where rn <= 3
+order by custid, rn;
+
+go
+
+select C.custid, A.orderdate, A.orderid, A.empid
+from Sales.Customers as C
+cross apply (select top (3) orderdate, orderid, empid
+    from Sales.Orders as O
+    where O.custid = C.custid
+    order by orderdate desc, orderid desc) as A
+go
+drop index if exists idx_cid_odD_oidD_i_empid on Sales.Orders
+go
+
+----
+
+drop table if exists dbo.T1;
+go
+
+create table dbo.T1(
+    id int not null constraint PK_T1 primary key,
+    col1 int null
+)
+
+insert into dbo.T1(id, col1) values
+(2, null),
+(3, 10),
+                                    (5, -1),
+                                    (7, null),
+                                    (11, null),
+                                    (13, -12),
+                                    (17, null),
+                                    (19, null),
+                                    (23, 1759)
+                                    
+                                    go
+
+with C as(
+    select id, col1, 
+           max(case when col1 is not null then id end)
+            over (order by id rows unbounded preceding ) as grp
+    from T1
+)
+select id, col1,
+       max(col1) over (partition by grp order by id
+           rows unbounded  preceding ) as lastValue
+from C
+option(use hint('DISALLOW_BATCH_MODE'))
+go
+truncate table T1
+select n as id, checksum(newid()) as col1
+from dbo.GetNums(1, 10000000) as Nums
+option(maxdop 1);
+go
+select id, col1, binstr,
+        max(binstr) over (order by id rows unbounded preceding) as mx
+from T1
+         cross apply (values(cast(id as binary(4)) + cast(col1 as binary(4)))) as A(binstr);
+
+go
+select id, col1, cast(substring(max(cast(id as binary(4)) + cast(col1 as binary(4))) over (order by id rows unbounded preceding), 5,4) as int) as lastval
+from T1
+
+;
+go
+create index idx_custid_empid on Sales.Orders(custid, empid);
+go
+with C as (
+    select custid,
+           empid,
+           count(*)                                                                   as cnt,
+           rank() over (partition by custid order by count(*) desc) as rn
+    from Sales.Orders
+    group by custid, empid
+)
+select * from C
+where rn = 1
+go
+drop index if exists idx_custid_empid on Sales.Orders
+go
+with C as(
+select empid, val,
+       ntile(20) over (partition by empid order by val) as ntile20
+from Sales.OrderValues)
+select empid, avg(val) as avgval
+from C
+where ntile20 between 2 and 19
+group by empid;
+go
+drop table if exists Transactions
+create table Transactions
+(
+    actid int not null,
+    tranid int not null,
+    val money not null,
+    constraint PK_Transactions Primary Key (actid, tranid)
+);
+go
+
+insert into Transactions(actid, tranid, val) values
+(1, 1, 4.00),
+(1, 2, -2.00),
+(1, 3, 5.00),
+(1, 4, 2.00),
+(1, 5, 1.00),
+(1, 6, 3.00),
+(1, 7, -4.00),
+(1, 8, -1.00),
+(1, 9, -2.00),
+(1, 10, -3.00),
+(2, 1, 2.00),
+(2, 2, 1.00),
+(2, 3, 5.00),
+(2, 4, 1.00),
+(2, 5, -5.00),
+(2, 6, 4.00),
+(2, 7, 2.00),
+(2, 8, -4.00),
+(2, 9, -5.00),
+(2, 10, 4.00),
+(3, 1, -3.00),
+(3, 2, 3.00),
+(3, 3, -2.00),
+(3, 4, 1.00),
+(3, 5, 4.00),
+(3, 6, -1.00),
+(3, 7, 5.00),
+(3, 8, 3.00),
+(3, 9, 5.00),
+(3, 10, -3.00);
+go
+select * from Transactions
+go
+create index idx_actid  on Transactions (actid, tranid) include (val)
+drop index if exists idx_actid on Transactions
+go
+declare @num_partitions  as int = 100,
+        @rows_per_partition as int = 10000;
+
+truncate table Transactions;
+insert into Transactions with (tablock) (actid, tranid, val)
+select NP.n, RPP.n,
+       (ABS(checksum(newid())%2)*2-1) * (1 + abs(checksum(newid())%5))
+from GetNums(1, @num_partitions) as NP
+cross join GetNums(1, @rows_per_partition) as rpp;
+go
+select actid, tranid, val,
+       sum(val) over (partition by actid 
+                        order by tranid 
+                        rows unbounded preceding) as balance
+from Transactions;
+
+select actid, tranid, val,
+       (select sum(t2.val) from Transactions as T2
+           where T2.actid = T1.actid and 
+                 T2.tranid <= T1.tranid) as balance
+from Transactions as T1;
+
+select T1.actid, T1.tranid, T1.val,
+       sum(T2.val) as balance
+from Transactions as T1
+inner join Transactions T2 on T1.actid = T2.actid 
+and T2.tranid   <= T1.tranid
+group by T1.actid, T1.tranid, T1.val;
+
+go
+declare @Result as table
+(
+    actid int,
+    tranid int,
+    val money,
+    balance money
+);
+
+declare 
+    @C As Cursor,
+    @actid as int,
+    @prvactid as int,
+    @tranid as int,
+    @val as money,
+    @balance as money;
+
+set @C = cursor forward_only static read_only for
+select actid, tranid, val
+from Transactions
+order by actid, tranid;
+
+open @C
+fetch next from @c into @actid, @tranid, @val;
+
+select @prvactid = @actid, @balance = 0;
+
+while @@fetch_status = 0 
+begin
+    if @actid <> @prvactid
+        select @prvactid = @actid, @balance = 0;
+        
+    set @balance = @balance + @val;
+    
+    insert into @Result values(@actid, @tranid, @val, @balance);
+    
+    fetcH next from @C into @actid, @tranid, @val;
+end
+
+select * from @Result;
+go
+select actid, tranid, val,
+       row_number() over(partition by actid order by tranid) as rownum
+into #Transactions
+from Transactions
+
+create unique clustered index idx_rownum_actid on #Transactions(rownum, actid);
+
+with C as(
+    select 1 as rownum, actid, tranid, val, val as sumqty
+    from #Transactions
+    where rownum = 1
+    
+    union all
+    select prv.rownum + 1, prv.actid, prv.tranid, cur.val, prv.sumqty + cur.val
+    from C as prv
+    inner join #Transactions as cur
+    on cur.rownum = prv.rownum+1
+    and cur.actid = prv.actid
+)
+select actid, tranid, val, sumqty
+from c
+option (maxrecursion 0);
